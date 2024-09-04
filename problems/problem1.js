c40883416fe7905964a7be2986365c51564e9497
akshay-bulkunde <akshay.bulkunde.29.3@mountblue.tech>
2024-09-04 20:34:11 +0530

    add problem1.js file

---
+++ problems/problem1.js
@@ -0,0 +1,20 @@
+// Given the dataset of individuals, write a function that accesses and returns the email addresses of all individuals.
+
+const {arrayOfObjects}  = require('../persons.js')
+
+
+function problem1(arrayOfObjects ){
+    //console.log(arrayOfObjects )
+    let emails = [];
+    for(let i=0;i<arrayOfObjects.length;i++){
+        //console.log(personData[0])
+        emails.push(arrayOfObjects [i].email)
+        //console.log(emails)
+    }
+    //console.log(emails);
+    return emails;
+}
+//console.log(problem1(arrayOfObjects ))
+module.exports = { problem1 };
+// const ans = problem1(data);
+// console.log(ans);
