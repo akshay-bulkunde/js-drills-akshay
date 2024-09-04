dc1663f9b2a4fdb410d0a39fda1ed39f7ffee408
akshay-bulkunde <akshay.bulkunde.29.3@mountblue.tech>
2024-09-04 20:42:01 +0530

    add testProblem2.js

---
+++ Test/testProblem2.js
@@ -0,0 +1,7 @@
+const { arrayOfObjects }   = require('../persons.js');
+const {problem2} = require('../problems/problem2.js');
+
+console.log(problem2(arrayOfObjects));
+console.log(problem1([]));
+console.log(problem1(' '));
+console.log(problem1());
