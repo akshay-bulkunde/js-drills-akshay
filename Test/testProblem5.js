942b2fb58407d38810e028078bd47ce660e9b18b
akshay-bulkunde <akshay.bulkunde.29.3@mountblue.tech>
2024-09-04 21:10:52 +0530

    add problem5.js and testProblem5.js file

---
+++ Test/testProblem5.js
@@ -0,0 +1,6 @@
+const {arrayOfObjects} = require('../persons.js');
+const {problem5} = require('../problems/problem5.js');
+
+problem5(arrayOfObjects);
+problem5([]);
+problem5(' ');
---
+++ problems/problem5.js
@@ -0,0 +1,9 @@
+//    Implement a loop to access and print the ages of all individuals in the dataset.
+
+function problem5(arrayOfObjects){
+    for(let i=0;i<arrayOfObjects.length;i++){
+        console.log(arrayOfObjects[i].age)
+    }
+}
+
+module.exports ={ problem5 };
