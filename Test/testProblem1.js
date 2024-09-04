d5fbd0010869a7d1399d1927dbedc522dd587cb4
akshay-bulkunde <akshay.bulkunde.29.3@mountblue.tech>
2024-09-04 20:38:51 +0530

    add testProblem1.js file

---
+++ Test/testProblem1.js
@@ -0,0 +1,8 @@
+const {arrayOfObjects } = require('../persons.js')
+const {problem1} = require('../problems/problem1.js')
+//console.log(problem1)
+
+console.log(problem1(arrayOfObjects ));
+console.log(problem1([]));
+console.log(problem1(' '));
+console.log(problem1());
