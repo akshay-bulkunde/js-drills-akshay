e13a2d81ece52d5032246d15d0aa664b0fcb8d43
akshay-bulkunde <akshay.bulkunde.29.3@mountblue.tech>
2024-09-05 17:01:38 +0530

    add problem2.js and testProblem2.js

---
+++ d3Problems/problem2.js
@@ -0,0 +1,13 @@
+// The dealer needs the information on the last car in their inventory. Execute a function to find what the make and model of the last car in the inventory is?  
+//Log the make and model into the console in the format of:
+//("Last car is a *car make goes here* *car model goes here*");
+
+function problem2(inventory){
+    
+        
+    console.log(`Last car is a ${inventory[inventory.length-1].car_make} ${inventory[inventory.length-1].car_model}`)
+        
+    
+}
+
+module.exports = {problem2}
