617284a3409307b83b414cc70ca1aa8a945e2bee
akshay-bulkunde <akshay.bulkunde.29.3@mountblue.tech>
2024-09-08 21:52:26 +0530

    add problem4.js and testProblem4.js

---
+++ d5Test/testProblem4.js
@@ -0,0 +1,5 @@
+const {pairs} = require('../d5Problems/problem4.js');
+const testObject = { name: "Bruce Wayne", age: 36, location: "Gotham" };
+
+let result = pairs(testObject);
+console.log(result);
