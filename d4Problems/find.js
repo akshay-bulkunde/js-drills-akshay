0e4e642ec1816d98d90c8f54631ea0cab415f240
akshay-bulkunde <akshay.bulkunde.29.3@mountblue.tech>
2024-09-08 19:17:55 +0530

    add find.js

---
+++ d4Problems/find.js
@@ -0,0 +1,27 @@
+const items = [1, 2, 3, 4, 5, 5];
+
+function find(elements, cb) {
+    let result = undefined;
+    for(let i=0;i<elements.length;i++){
+        if(cb(elements[i])){
+            result = elements[i];
+            //break;
+        }
+        
+    }
+    return result;
+}
+
+function callback(element){
+   return element === 3;
+}
+
+function callback1(element){
+    return element === 6;
+ }
+
+let result = find(items, callback);
+// console.log(result);
+
+let ans  = find(items , callback1);
+console.log(ans);
